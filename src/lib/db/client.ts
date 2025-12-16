ded1b5020f58f11900e98901d7d07be88f76bbd5
Martin Křivda <martin.krivda@kobchocen.cz>
2025-12-16 10:58:43 +0100

    chore: lint warning

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -2,7 +2,6 @@ import { env } from "@/lib/env";
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // eslint-disable-next-line no-var, vars-on-top
   var prisma: PrismaClient | undefined;
 }
 
